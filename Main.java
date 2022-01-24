b20396eefe7d2f4de1f041eaa898d6e6240f692f
Delta_wasabi <65533242+tristaneiche@users.noreply.github.com>
2022-01-24 09:50:00 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package save_GOSecuri;
+import java.awt.Desktop;
+import java.io.*;
+
+class main {
+
+    public static void main(String[] args) throws Exception {
+        File f = new File("source.html");
+        BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+        bw.write("<html><body>");
+        bw.write("<div>");
+        bw.write("<p>OwO</p>");
+        bw.write("<p>UwU</p>");
+        bw.write("</div>");
+        bw.write("</body></html>");
+        bw.close();
+    }
+}
