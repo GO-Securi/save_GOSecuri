76d5564b911d4e710b1c0c6e57782b8d0ecba41a
Guillaume Tauzin <guillaumetauzin@icloud.com>
2022-01-28 10:20:00 +0100

    update

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("test");
+    public static void main(String[] args) throws IOException {
+        String file = "my-file.txt";
+        try(BufferedReader br = new BufferedReader(new FileReader(file))) 
+        {
+            String line;
+            while ((line = br.readLine()) != null) {
+            System.out.println(line);
+            }
+        }
+        catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        System.out.println("Hello world!");
     }
 }
