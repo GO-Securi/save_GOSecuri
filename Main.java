39ef798a8b541b07db8bac58e57a0ee12d93d387
Guillaume Tauzin <guillaumetauzin@icloud.com>
2022-02-15 22:56:50 +0100

    .
    Co-authored-by: 775pl <775pl@users.noreply.github.com>

--- Main.java
+++ Main.java
@@ -2,31 +2,38 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Debut script");
         String path = "";
 
+
         //Lecture du fichier staff.txt
-        try {
-            System.out.println("<!DOCTYPE html><html><head><link href=\"Style.css\" rel=\"stylesheet\" type=\"text/css\"></head><body><header><img class=\"logo\" src=\"logo.png\" alt=\"Logo de l'entreprise\"></header><ul class=\"agents\">");
+        try (FileWriter writer = new FileWriter(new File(path + "html\\agents.html"));
+        BufferedWriter bw = new BufferedWriter(writer)){
+
+
+            bw.write("<!DOCTYPE html><html><head><link href=\"Style.css\" rel=\"stylesheet\" type=\"text/css\"></head><body><header><img class=\"logo\" src=\"logo.png\" alt=\"Logo de l'entreprise\"></header><ul class=\"agents\">");
+
             BufferedReader staff = new BufferedReader(new FileReader(new File(path + "datas\\staff.txt")));
             String line_staff = staff.readLine();
             while (line_staff != null) {
-                System.out.print("<li class=\"agent\" ><a href=\""+ line_staff +".html\">");
+                bw.write("<li class=\"agent\" ><a href=\""+ line_staff +".html\">");
                 BufferedReader agent = new BufferedReader(new FileReader(new File(path + "datas\\"+ line_staff +".txt")));
                 String line_agent = agent.readLine();
                 int i = 0;
                 while (i <= 1) {
                     i++;
-                    System.out.print(line_agent + " ");
+                    bw.write(line_agent + " ");
                     line_agent = agent.readLine();
                 }
-                System.out.println("</a></li>");
+                bw.write("</a></li>");
                 line_staff = staff.readLine();
             }
-            System.out.println("</ul><footer class=\"footer\"><p class=\"mentions\">Mentions légales</p></footer></body></html>");
+            bw.write("</ul><footer class=\"footer\"><p class=\"mentions\">Mentions légales</p></footer></body></html>");
         } catch (IOException e) {
             e.printStackTrace();
         }
