d28101b3df836f336209e3014b3a15e93d0a9b5e
Guillaume Tauzin <guillaumetauzin@icloud.com>
2022-03-24 22:32:53 +0100

    commit

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+
 import java.io.FileWriter;
 import java.io.BufferedWriter;
 
@@ -13,13 +14,13 @@ public class Main {
         System.out.println("Debut generation liste agents");
         String path = "";
 
-        //Lecture du fichier staff.txt
+        //Ecriture du fichier agents.html (liste des agents)
         try (FileWriter writer = new FileWriter(new File(path + "html\\agents.html"));
         BufferedWriter bw = new BufferedWriter(writer)){
 
 
             bw.write("<!DOCTYPE html><html><head><link href=\"Style.css\" rel=\"stylesheet\" type=\"text/css\"></head><body><header><img class=\"logo\" src=\"logo.png\" alt=\"Logo de l'entreprise\"></header><ul class=\"agents\">");
-
+            //Lecture du fichier staff.txt
             BufferedReader staff = new BufferedReader(new FileReader(new File(path + "datas\\staff.txt")));
             String line_staff = staff.readLine();
 
@@ -27,11 +28,25 @@ public class Main {
 
             while (line_staff != null) {
 
-                try (FileWriter writer2 = new FileWriter(new File(path + line_staff + "\\" + line_staff + "agents.html"));
+
+                bw.write("<li class=\"agent\" ><a href=\""+ line_staff + "\\" + "index.html\">");
+                BufferedReader agent = new BufferedReader(new FileReader(new File(path + "datas\\"+ line_staff +".txt")));
+                //etape2
+                //Pour chaque agents on cree un fichier + htacces:
+                System.out.println(path + "html\\" + line_staff + "\\" + "index.html");
+                File dossier = new File(path + "html\\" + line_staff); 
+                dossier.mkdir();
+                try (FileWriter writer2 = new FileWriter(new File(path + "html\\"+line_staff+"\\index.html"));
                 BufferedWriter bw_fiche_agent = new BufferedWriter(writer2)){
 
-                bw.write("<li class=\"agent\" ><a href=\""+ line_staff + "\\" + line_staff + ".html\">");
-                BufferedReader agent = new BufferedReader(new FileReader(new File(path + "datas\\"+ line_staff +".txt")));
+                    //Header
+                    bw_fiche_agent.write("<!DOCTYPE html><html><head><link href=\"Style.css\" rel=\"stylesheet\" type=\"text/css\"></head><body><header><img class=\"logo\" src=\"logo.png\" alt=\"Logo de l'entreprise\"></header");
+
+
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+
                 String line_agent = agent.readLine();
                 int i = 0; 
                 while (i <= 1) {
@@ -41,24 +56,6 @@ public class Main {
                 }
                 bw.write("</a></li>");
                 line_staff = staff.readLine();
-
-                //etape 2
-
-    bw_fiche_agent.write("<!DOCTYPE html><html><head><link href=\"Style.css\" rel=\"stylesheet\" type=\"text/css\"></head><body><header><img class=\"logo\" src=\"logo.png\" alt=\"Logo de l'entreprise\"></header");
-
-
-
-
-                
-
-                
-                    
-             
-        
-                    
-                }catch (IOException e) {
-                    e.printStackTrace();
-                }
             }
             bw.write("</ul><footer class=\"footer\"><p class=\"mentions\">Mentions légales</p></footer></body></html>");
         } catch (IOException e) {
